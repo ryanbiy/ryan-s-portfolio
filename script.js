e018da39133fcb2a02bc37eec49d9669f9f823b5
Ryan Biy <ryanbiy01.com>
2023-09-29 20:39:31 +0300

    updated cv

--- script.js
+++ script.js
@@ -4,8 +4,4 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-const switchElement = document.querySelector('.switch')
-
-switchElement.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
-})
+;
